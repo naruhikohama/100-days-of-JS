640e2c8e12d05258b5ba8f6b9644a93229203c51
naruhikohama <naruhikohama@gmail.com>
2022-05-18 21:36:10 -0300

    Day 13 - HTTP Requests local

---
+++ Day 13 - HTTP Request/ajax.js
@@ -0,0 +1,37 @@
+// Load users
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", getUsers);
+
+// Create function getUsers
+function getUsers(e) {
+    e.preventDefault();
+
+    const http = new XMLHttpRequest();
+
+    http.open("GET", "users.json", true);
+
+    http.onload = function() {
+        if (this.status === 200) {
+            // console.log(this.responseText);
+            const users = JSON.parse(this.responseText);
+
+            let output = "";
+            users.forEach(function(user) {
+                output += `
+                <hr>
+                <ul>
+                    <li>ID: ${user.id}</li>
+                    <li>Name: ${user.name}</li>
+                    <li>Age: ${user.age}</li>
+                    <li>email: ${user.email}</li>
+                </ul>
+                `;
+            })
+
+            document.getElementById("users").innerHTML = output;
+        }
+    };
+
+    http.send();
+}
